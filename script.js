914612cb833e2b06ef12fb43d24eb01471daed78
Nicolas de Oliveira Pacheco <104364831+NicolasOliveiraP@users.noreply.github.com>
2024-11-12 20:19:26 -0300

    commitarDps

---
+++ script.js
@@ -0,0 +1,13 @@
+const campoLogin = document.getElementById("login");
+const campoSenha = document.getElementById("password");
+const campoNovoLogin = document.getElementById("novoLogin");
+const campoNovaSenha = document.getElementById("novaSenha");
+const campoRepSenha = document.getElementById("repSenha");
+
+function login(){
+
+}
+
+function cadastra(){
+    
+}
