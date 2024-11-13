974b8153e89a624c755c4dab517df1e175ba1a6e
NICOLAS DE OLIVEIRA PACHECO <nicolas_o_pacheco@estudante.sesisenai.org.br>
2024-11-12 21:50:03 -0300

    Alterações bases

---
+++ script.js
@@ -0,0 +1,44 @@
+const campoLogin = document.getElementById("login");
+const campoSenha = document.getElementById("password");
+const campoNovoLogin = document.getElementById("novoLogin");
+const campoNovaSenha = document.getElementById("novaSenha");
+const campoRepSenha = document.getElementById("repSenha");
+
+function login(){
+    let login = campoLogin.value;
+    let senha = campoSenha.value;
+    let mensagem = "Usuário ou senha incorreta!";
+    let bancoDeDados = JSON.parse(localStorage.getItem("bancodedados"));
+    if (bancoDeDados == null) {
+        mensagem = "Nenhum usuário cadastrado até o momento";
+    } else {
+        for (let usuario of bancoDeDados) {
+            if (usuario.login == login && usuario.senha == senha) { //O usuario.login faz com que
+                mensagem = "Parabéns, você logou!";
+                localStorage.setItem("logado", JSON.stringify(usuario));
+                break;
+            }
+        }
+    }
+    alert(mensagem)
+}
+
+function cadastra(){
+    if (campoNovaSenha.value == campoRepSenha.value) {
+        const usuario = {
+            login: campoNovoLogin.value,
+            senha: campoNovaSenha.value
+        };
+        let bancoDeDados = JSON.parse(localStorage.getItem("bancoDeDados"));
+        if (bancoDeDados == null) {
+            bancoDeDados = [];
+        }
+        
+    bancoDeDados.push(usuario);
+    localStorage.setItem("bancoDeDados", JSON.stringify(bancoDeDados));
+    alert("usuário cadastrado com sucesso!")
+    } else {
+    alert("As senhas são diferentes!");
+    }
+    
+}
